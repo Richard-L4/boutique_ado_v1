07913bf8843da1dd346a062e98df93f955a44343
Richard-L4 <rwells945@gmail.com>
2025-05-26 12:15:59 +0100

    add / delete products page added

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -110,6 +110,7 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
 
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
