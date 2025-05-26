d5f5851eb3d1a56d9fe44f56c76306d231075978
Richard-L4 <rwells945@gmail.com>
2025-05-26 15:05:15 +0100

    again

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ DEBUG = os.environ.get("DEVELOPMENT") == "1"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
