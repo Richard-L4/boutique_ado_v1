159ca676fa2291c3eb21c3233883cd2af216575a
Richard-L4 <rwells945@gmail.com>
2025-05-27 09:12:04 +0100

    add bug fix

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,10 +6,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-const stripePublicKey = JSON.parse(document.getElementById('id_stripe_public_key').textContent);
-const clientSecret = JSON.parse(document.getElementById('id_client_secret').textContent);
-const stripe = Stripe(stripePublicKey);
-
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
